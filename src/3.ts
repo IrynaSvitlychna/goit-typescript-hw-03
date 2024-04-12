69e334d9e54f94d6255a2e64d24ebdf4d89ddedc
IrynaSvitlychna <139795252+IrynaSvitlychna@users.noreply.github.com>
2024-04-12 22:33:49 +0300

    has finished HW3

--- src/3.ts
+++ src/3.ts
@@ -7,7 +7,20 @@
   Інтерфейс ISpellCaster повинен включати метод castSpell.
 */
 
-// реалізація класу Wizard
+
+interface ICharacter {
+  name: string;
+  level: number;
+
+  introduce(phrase: string): void;
+  levelUp(): void;
+}
+  
+interface ISpellCaster {
+  castSpell(): void;
+}
+  
+ // реалізація класу Wizard 
 class Wizard implements ICharacter, ISpellCaster {
   constructor(public name: string, public level: number) {
     if (this.level < 1) {
